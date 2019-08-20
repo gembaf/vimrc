12be52e473b5aeed0fe3284646191344ab7af174
fumiya.gemba <fumiya.gemba@20204114n.intra.emercury.co.jp>
2019-08-20 17:58:56 +0900

    cp ではなくリダイレクトで追記する

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/.bundle/neobundle.vim
 vim +":NeoBundleInstall" +":q"
-cp -i <(echo 'source ~/.vim/.vimrc') ~/.vimrc
+echo 'source ~/.vim/.vimrc' >> ~/.vimrc
 
